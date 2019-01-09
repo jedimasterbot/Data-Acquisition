e1319e9f623f4dbcdfc1505056e563d93e8e97b9
Muhammad Haseeb Rafi <m.haseeb.rafi@gmail.com>
2019-01-09 18:21:17 +0500

    Initial commit

---
+++ Project/Form1.Designer.vb
@@ -0,0 +1,360 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtoutput = New System.Windows.Forms.TextBox()
+        Me.txtreading = New System.Windows.Forms.TextBox()
+        Me.txtcurrent = New System.Windows.Forms.TextBox()
+        Me.txtreadingleft = New System.Windows.Forms.TextBox()
+        Me.btnstart = New System.Windows.Forms.Button()
+        Me.btnexit = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.txtdate = New System.Windows.Forms.TextBox()
+        Me.txttime = New System.Windows.Forms.TextBox()
+        Me.txtcom = New System.Windows.Forms.ComboBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.btnconnect = New System.Windows.Forms.Button()
+        Me.Timer_LBL = New System.Windows.Forms.Label()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.connecting_Timer = New System.Windows.Forms.Timer(Me.components)
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.txtrich = New System.Windows.Forms.Label()
+        Me.showData = New System.Windows.Forms.Button()
+        Me.clr = New System.Windows.Forms.Button()
+        Me.sav = New System.Windows.Forms.Button()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 88)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(92, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Outout File Name:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 138)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(82, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "No. of Reading:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 188)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(70, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Current Data:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(7, 243)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(108, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Readings Remaining:"
+        '
+        'txtoutput
+        '
+        Me.txtoutput.Location = New System.Drawing.Point(136, 81)
+        Me.txtoutput.Name = "txtoutput"
+        Me.txtoutput.Size = New System.Drawing.Size(151, 20)
+        Me.txtoutput.TabIndex = 5
+        '
+        'txtreading
+        '
+        Me.txtreading.Location = New System.Drawing.Point(136, 131)
+        Me.txtreading.Name = "txtreading"
+        Me.txtreading.Size = New System.Drawing.Size(151, 20)
+        Me.txtreading.TabIndex = 7
+        '
+        'txtcurrent
+        '
+        Me.txtcurrent.Location = New System.Drawing.Point(136, 181)
+        Me.txtcurrent.Name = "txtcurrent"
+        Me.txtcurrent.Size = New System.Drawing.Size(151, 20)
+        Me.txtcurrent.TabIndex = 8
+        '
+        'txtreadingleft
+        '
+        Me.txtreadingleft.Location = New System.Drawing.Point(136, 236)
+        Me.txtreadingleft.Name = "txtreadingleft"
+        Me.txtreadingleft.Size = New System.Drawing.Size(151, 20)
+        Me.txtreadingleft.TabIndex = 9
+        '
+        'btnstart
+        '
+        Me.btnstart.Location = New System.Drawing.Point(48, 331)
+        Me.btnstart.Name = "btnstart"
+        Me.btnstart.Size = New System.Drawing.Size(92, 28)
+        Me.btnstart.TabIndex = 10
+        Me.btnstart.Text = "Start"
+        Me.btnstart.UseVisualStyleBackColor = True
+        '
+        'btnexit
+        '
+        Me.btnexit.Location = New System.Drawing.Point(48, 380)
+        Me.btnexit.Name = "btnexit"
+        Me.btnexit.Size = New System.Drawing.Size(92, 28)
+        Me.btnexit.TabIndex = 11
+        Me.btnexit.Text = "Exit"
+        Me.btnexit.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'Chart1
+        '
+        ChartArea1.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea1)
+        Legend1.Name = "Legend1"
+        Me.Chart1.Legends.Add(Legend1)
+        Me.Chart1.Location = New System.Drawing.Point(307, 81)
+        Me.Chart1.Name = "Chart1"
+        Me.Chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright
+        Series1.ChartArea = "ChartArea1"
+        Series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line
+        Series1.Legend = "Legend1"
+        Series1.MarkerColor = System.Drawing.Color.Transparent
+        Series1.Name = "Series1"
+        Me.Chart1.Series.Add(Series1)
+        Me.Chart1.Size = New System.Drawing.Size(595, 278)
+        Me.Chart1.TabIndex = 12
+        Me.Chart1.Text = "Chart1"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(540, 380)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(81, 13)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "No.of Readings"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(411, 54)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(33, 13)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "Date:"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(648, 54)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(33, 13)
+        Me.Label9.TabIndex = 16
+        Me.Label9.Text = "Time:"
+        '
+        'txtdate
+        '
+        Me.txtdate.Location = New System.Drawing.Point(450, 51)
+        Me.txtdate.Name = "txtdate"
+        Me.txtdate.Size = New System.Drawing.Size(171, 20)
+        Me.txtdate.TabIndex = 17
+        '
+        'txttime
+        '
+        Me.txttime.Location = New System.Drawing.Point(687, 51)
+        Me.txttime.Name = "txttime"
+        Me.txttime.Size = New System.Drawing.Size(171, 20)
+        Me.txttime.TabIndex = 18
+        '
+        'txtcom
+        '
+        Me.txtcom.FormattingEnabled = True
+        Me.txtcom.Items.AddRange(New Object() {"COM1", "COM2"})
+        Me.txtcom.Location = New System.Drawing.Point(10, 38)
+        Me.txtcom.Name = "txtcom"
+        Me.txtcom.Size = New System.Drawing.Size(153, 21)
+        Me.txtcom.TabIndex = 19
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(59, 22)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(56, 13)
+        Me.Label10.TabIndex = 20
+        Me.Label10.Text = "COM Port:"
+        '
+        'btnconnect
+        '
+        Me.btnconnect.Location = New System.Drawing.Point(177, 31)
+        Me.btnconnect.Name = "btnconnect"
+        Me.btnconnect.Size = New System.Drawing.Size(110, 28)
+        Me.btnconnect.TabIndex = 22
+        Me.btnconnect.Text = "Connect"
+        Me.btnconnect.UseVisualStyleBackColor = True
+        '
+        'Timer_LBL
+        '
+        Me.Timer_LBL.AutoSize = True
+        Me.Timer_LBL.Location = New System.Drawing.Point(309, 34)
+        Me.Timer_LBL.Name = "Timer_LBL"
+        Me.Timer_LBL.Size = New System.Drawing.Size(62, 13)
+        Me.Timer_LBL.TabIndex = 23
+        Me.Timer_LBL.Text = "Timer : OFF"
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.PortName = "COM3"
+        '
+        'connecting_Timer
+        '
+        Me.connecting_Timer.Interval = 250
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(915, 66)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(155, 353)
+        Me.RichTextBox1.TabIndex = 25
+        Me.RichTextBox1.Text = ""
+        '
+        'txtrich
+        '
+        Me.txtrich.AutoSize = True
+        Me.txtrich.Location = New System.Drawing.Point(953, 50)
+        Me.txtrich.Name = "txtrich"
+        Me.txtrich.Size = New System.Drawing.Size(79, 13)
+        Me.txtrich.TabIndex = 26
+        Me.txtrich.Text = "Received Data"
+        '
+        'showData
+        '
+        Me.showData.Location = New System.Drawing.Point(166, 331)
+        Me.showData.Name = "showData"
+        Me.showData.Size = New System.Drawing.Size(91, 28)
+        Me.showData.TabIndex = 27
+        Me.showData.Text = "Show"
+        Me.showData.UseVisualStyleBackColor = True
+        '
+        'clr
+        '
+        Me.clr.Location = New System.Drawing.Point(956, 425)
+        Me.clr.Name = "clr"
+        Me.clr.Size = New System.Drawing.Size(92, 28)
+        Me.clr.TabIndex = 28
+        Me.clr.Text = "Clear"
+        Me.clr.UseVisualStyleBackColor = True
+        '
+        'sav
+        '
+        Me.sav.Location = New System.Drawing.Point(165, 380)
+        Me.sav.Name = "sav"
+        Me.sav.Size = New System.Drawing.Size(92, 28)
+        Me.sav.TabIndex = 29
+        Me.sav.Text = "Save"
+        Me.sav.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1082, 467)
+        Me.Controls.Add(Me.sav)
+        Me.Controls.Add(Me.clr)
+        Me.Controls.Add(Me.showData)
+        Me.Controls.Add(Me.txtrich)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Timer_LBL)
+        Me.Controls.Add(Me.btnconnect)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.txtcom)
+        Me.Controls.Add(Me.txttime)
+        Me.Controls.Add(Me.txtdate)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Chart1)
+        Me.Controls.Add(Me.btnexit)
+        Me.Controls.Add(Me.btnstart)
+        Me.Controls.Add(Me.txtreadingleft)
+        Me.Controls.Add(Me.txtcurrent)
+        Me.Controls.Add(Me.txtreading)
+        Me.Controls.Add(Me.txtoutput)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtoutput As System.Windows.Forms.TextBox
+    Friend WithEvents txtreading As System.Windows.Forms.TextBox
+    Friend WithEvents txtcurrent As System.Windows.Forms.TextBox
+    Friend WithEvents txtreadingleft As System.Windows.Forms.TextBox
+    Friend WithEvents btnstart As System.Windows.Forms.Button
+    Friend WithEvents btnexit As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Chart1 As System.Windows.Forms.DataVisualization.Charting.Chart
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents txtdate As System.Windows.Forms.TextBox
+    Friend WithEvents txttime As System.Windows.Forms.TextBox
+    Friend WithEvents txtcom As System.Windows.Forms.ComboBox
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents btnconnect As System.Windows.Forms.Button
+    Friend WithEvents Timer_LBL As System.Windows.Forms.Label
+    Friend WithEvents connecting_Timer As System.Windows.Forms.Timer
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents txtrich As System.Windows.Forms.Label
+    Public WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents showData As System.Windows.Forms.Button
+    Friend WithEvents clr As System.Windows.Forms.Button
+    Friend WithEvents sav As System.Windows.Forms.Button
+
+End Class
---
+++ Project/Form1.vb
@@ -0,0 +1,293 @@
+﻿
+Imports System
+Imports System.IO.Ports
+Imports System.Windows.Forms.DataVisualization.Charting.Chart
+Imports System.Windows.Forms.DataVisualization.Charting
+Imports Microsoft.Office.Interop
+Imports System.IO
+'My.Computer.FileSystem.WriteAllText("C:\TestFolder1\test.txt","This is new text to be added.",True)
+
+Public Class Form1
+    'Global variables
+    Dim s As New Series
+    Dim data As String
+    Dim i As Integer = -1
+    Dim j As Integer = 0
+    Dim f As Integer = -1
+    Dim array(50) As String
+    Dim c1 As String
+    Dim voltage As Double
+    Dim y As Double
+    Dim s2 As String
+    Dim comPORT As String
+    Dim receivedData As String = ""
+    Dim connected As Boolean = False
+    Dim stp As Boolean = False
+    Dim count = 0
+    Dim Incoming1 As String
+    Dim a As String
+    Dim b As String
+    Dim c As String
+    Dim d As String
+    Dim con As Double
+    Dim inputString As String = "Data Record:" & vbCrLf
+
+    Dim fileTest As String = "E:\Temp\ExcelTest\\testfile.txt"
+
+    Dim oExcel As Object
+    Dim oBook As Excel.Workbook
+    Dim oSheet As Excel.Worksheet
+    Dim Rng As Excel.Range
+    Dim StartRow, StartCol
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtoutput.Text = "testfile"
+        Me.Chart1.Series("Series1").BorderWidth = 5
+        If File.Exists(fileTest) Then
+            File.Delete(fileTest)
+        End If
+        txtdate.Text = DateTime.Now.ToLongDateString()
+        txttime.Text = DateTime.Now.ToString("hh:mm")
+        Timer1.Enabled = False
+        populateCOMport()
+    End Sub
+    Private Sub populateCOMport()
+        comPORT = ""
+        txtcom.Items.Clear()
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            txtcom.Items.Add(sp)
+        Next
+    End Sub
+    Private Sub txtcom_SelectedIndexChanged(sender As Object, e As EventArgs) Handles txtcom.SelectedIndexChanged
+        If (txtcom.SelectedItem <> "") Then
+            comPORT = txtcom.SelectedItem
+        End If
+    End Sub
+    Private Sub btnconnect_Click(sender As Object, e As EventArgs) Handles btnconnect.Click
+        comPORT = txtcom.SelectedItem
+        If (btnconnect.Text = "Connect") Then
+            If (comPORT <> "") Then
+                SerialPort1.Close()
+                Console.WriteLine("zdsfdxtghfh")
+                SerialPort1.PortName = comPORT
+                SerialPort1.BaudRate = 9600
+                SerialPort1.DataBits = 8
+                SerialPort1.Parity = IO.Ports.Parity.None
+                SerialPort1.StopBits = IO.Ports.StopBits.One
+                SerialPort1.Handshake = IO.Ports.Handshake.None
+                SerialPort1.Encoding = System.Text.Encoding.Default
+                SerialPort1.ReadTimeout = 10000
+                SerialPort1.Open()
+                'See if the Arduino is there
+                count = 0
+                SerialPort1.WriteLine("<HELLO>")
+                btnconnect.Text = "Connecting..."
+                connecting_Timer.Enabled = True
+                txtreading.Text = "10"
+            Else
+                MsgBox("Select a COM port first")
+            End If
+        Else
+            'connect_BTN.Text = "Dis-connect"
+            'close the connection a reset the button and timer label
+            Timer1.Enabled = False
+            Timer_LBL.Text = "Timer: OFF"
+            SerialPort1.Close()
+            connected = False
+            btnconnect.Text = "Connect"
+            populateCOMport()
+        End If
+    End Sub
+    Private Sub connecting_Timer_Tick(sender As Object, e As EventArgs) Handles connecting_Timer.Tick
+        connecting_Timer.Enabled = False
+        count = count + 1
+        If (count <= 8) Then
+            receivedData = receivedData & ReceiveSerialData()
+            If (Microsoft.VisualBasic.Left(receivedData, 5) = "HELLO") Then
+                'if we get an HELLO from the Arduino then we are connected
+                connected = True
+                btnconnect.Text = "Dis-connect"
+                Timer1.Enabled = True
+                Timer_LBL.Text = "Timer: ON"
+                receivedData = ReceiveSerialData()
+                receivedData = ""
+                SerialPort1.WriteLine("<START>")
+            Else
+                'start the timer again and keep waiting for a signal from the Arduino
+                connecting_Timer.Enabled = True
+            End If
+        Else
+            'time out (8 * 250 = 2 seconds)
+            RichTextBox1.Text &= vbCrLf & "ERROR" & vbCrLf & "Can not connect" & vbCrLf
+            btnconnect.Text = "Connect"
+            populateCOMport()
+        End If
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        receivedData = ReceiveSerialData()
+        RichTextBox1.Text &= receivedData
+    End Sub
+    Function ReceiveSerialData() As String
+        Dim Incoming As String
+        Try
+            Incoming = SerialPort1.ReadExisting()
+            If Incoming Is Nothing Then
+                Return "nothing" & vbCrLf
+            Else
+                Return Incoming
+            End If
+        Catch ex As TimeoutException
+            Return "Error: Serial Port read timed out."
+        End Try
+    End Function
+    Function FunctionEval2(ByVal X As Double) As Double
+        FunctionEval2 = voltage
+    End Function
+    Private Sub Chart1_Click(sender As Object, e As EventArgs) Handles Chart1.Click
+    End Sub
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+    End Sub
+    Private Sub Label7_Click(sender As Object, e As EventArgs)
+    End Sub
+    Private Sub Label2_Click(sender As Object, e As EventArgs)
+    End Sub
+    Private Sub btnexit_Click(sender As Object, e As EventArgs) Handles btnexit.Click
+        Me.Close()
+    End Sub
+    Private Sub txtoutput_TextChanged(sender As Object, e As EventArgs) Handles txtoutput.TextChanged
+    End Sub
+    Private Sub txtcount_TextChanged(sender As Object, e As EventArgs)
+    End Sub
+    Private Sub txtreading_TextChanged(sender As Object, e As EventArgs) Handles txtreading.TextChanged
+
+    End Sub
+    Private Sub txtcurrent_TextChanged(sender As Object, e As EventArgs) Handles txtcurrent.TextChanged
+    End Sub
+    Private Sub txtreadingleft_TextChanged(sender As Object, e As EventArgs) Handles txtreadingleft.TextChanged
+    End Sub
+    Private Sub txtgraphtime_TextChanged(sender As Object, e As EventArgs)
+    End Sub
+    Private Sub txttime_TextChanged(sender As Object, e As EventArgs) Handles txttime.TextChanged
+    End Sub
+    Private Sub txtdate_TextChanged(sender As Object, e As EventArgs) Handles txtdate.TextChanged
+    End Sub
+    Private Sub Timer_LBL_Click(sender As Object, e As EventArgs) Handles Timer_LBL.Click
+    End Sub
+    Private Sub btnstart_Click(sender As Object, e As EventArgs) Handles btnstart.Click
+        If ((connected) And (stp = False)) Then
+            If (btnstart.Text = "Start") Then
+                SerialPort1.WriteLine("<P004ON>")
+                btnstart.Text = "Stop"
+                stp = True
+                j = 1
+                While j <= Convert.ToInt16(txtreading.Text) 'usman ka code ki pheli line
+                    txtreadingleft.Text = txtreading.Text - j
+                    j += 1
+                    wait(0.6)
+                    Incoming1 = SerialPort1.ReadChar()
+                    ' MsgBox(Incoming1)
+                    If ((Incoming1 = 65)) Then
+                        ' MsgBox("INCOMING2")
+                        Saving()
+                    End If
+                End While
+                SerialPort1.WriteLine("<P004OF>")
+                btnstart.Text = "Start"
+                stp = False
+            End If
+        ElseIf ((connected) And (stp = True)) Then
+            SerialPort1.WriteLine("<P004OF>")
+            btnstart.Text = "Start"
+            stp = False
+        End If
+        If (connected = False) Then
+            MsgBox("Not connected")
+        End If
+    End Sub
+    Function Saving() As Double
+        ' MsgBox("INCOMING3")
+        a = SerialPort1.ReadByte()
+        ' MsgBox(a)
+        b = SerialPort1.ReadByte()
+        'MsgBox(b)
+        c = a + "." + b
+        ' MsgBox(c)
+        txtcurrent.Text = c
+        inputString &= c & vbCrLf
+        con = Convert.ToDouble(c)
+        f = f + 1
+        Me.Chart1.Series("Series1").Points.AddXY(f, con)
+        'MsgBox(con)
+        '  Dim sensorValue As Double = SerialPort1.ReadByte()
+        '  voltage = sensorValue * (5.0 / 1023.0)
+        '  MsgBox(Convert.ToString(voltage))
+        Showing()
+        Return con
+    End Function
+    Function Showing() As String
+        i = i + 1
+        array(i) = con
+        Return array(i)
+    End Function
+    Private Sub showData_Click(sender As Object, e As EventArgs) Handles showData.Click
+        ' For j As Integer = 0 To array.Length
+        ' If (j = array.Length) Then
+        'Exit For
+        ' End If
+        ' Console.WriteLine(array(j))
+        ' Next
+
+        Dim result As New System.Text.StringBuilder
+        Dim j As Integer
+        For j = 0 To array.Length - 1
+            result = result.Append(Convert.ToString(array(j)))
+            If (j < (array.Length - 1)) Then result.Append(",")
+        Next j
+        MsgBox(Convert.ToString(result))
+        ' Console.WriteLine(result.ToString()) *\
+    End Sub
+    Private Sub wait(ByVal seconds As Double)
+        For i As Double = 0 To seconds * 100
+            System.Threading.Thread.Sleep(10)
+            Application.DoEvents()
+        Next
+    End Sub
+    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox1.TextChanged
+    End Sub
+
+    Private Sub clr_Click(sender As Object, e As EventArgs) Handles clr.Click
+        RichTextBox1.Text = ""
+        'Chart1.ChartAreas.Clear()
+    End Sub
+
+    Private Sub sav_Click(sender As Object, e As EventArgs) Handles sav.Click
+       
+        My.Computer.FileSystem.WriteAllText("E:\Temp\ExcelTest\\testfile.txt", inputString, False)
+        If (Not (txtoutput.Text = "testfile")) Then
+            My.Computer.FileSystem.RenameFile("E:\Temp\ExcelTest\\testfile.txt", txtoutput.Text & ".txt")
+        End If
+
+
+        'oExcel = CreateObject("Excel.Application")
+        'oBook = oExcel.Workbooks.Add
+        'oSheet = oExcel.Worksheets(1)
+        'oSheet.Name = "Test Name"
+        '  Dim g As Integer
+        'For g = 0 To f
+        'oSheet.Range("A1").Value = "some value"    
+        'Next g
+        'StartRow = 1
+        'With oSheet
+        ' Rng = .Range(.Cells(StartRow, "A"), _
+        '        .Cells(UBound(array, 1) - LBound(array, 1) + StartRow, _
+        '              UBound(array, 2) - LBound(array, 2) + "A"))
+        'End With
+        ' Rng.Value = array ' Column
+        'oBook.SaveAs(fileTest)
+        'oBook.Close()
+        'oBook = Nothing
+        '  oExcel.Quit()
+        ' oExcel = Nothing
+    End Sub
+   
+End Class
